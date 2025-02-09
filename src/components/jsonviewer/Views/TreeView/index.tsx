7374acabf2b935adc3b104e5ece8c2b928449cd7
sahilsinghrana <ranasahil98@gmail.com>
2025-02-10 01:05:31 +0530

    init

--- src/components/jsonviewer/Views/TreeView/index.tsx
+++ src/components/jsonviewer/Views/TreeView/index.tsx
@@ -1,3 +1,4 @@
+import { ALargeSmall, Braces, Brackets, Hash } from "lucide-react";
 import useViewerStore from "../../store/viewerStore";
 
 function TreeView() {
@@ -5,8 +6,8 @@ function TreeView() {
   return (
     <div>
       Viewer
-      <div>
-        <Tree objectKey="" value={parsedJson} />
+      <div className="p-2 outline-2 outline outline-gray-500 m-2 text-sm min-h-80">
+        <Tree objectKey="JSON" value={parsedJson} />
       </div>
     </div>
   );
@@ -17,22 +18,33 @@ export default TreeView;
 function Tree({ objectKey, value }: { objectKey: string; value: any }) {
   if (typeof value === "object") {
     if (Array.isArray(value)) {
-      return <div className="ml-2">{objectKey}: Array</div>;
+      return (
+        <details className="flex ml-2">
+          <summary className="cursor-pointer ">{objectKey}</summary>
+          {value.map(([k, v]) => (
+            <Tree key={k} objectKey={k} value={v} />
+          ))}
+        </details>
+      );
     }
     return (
-      <div>
+      <details className="flex ml-2">
+        <summary className="cursor-pointer ">{objectKey}</summary>
         {Object.entries(value).map(([k, v]) => (
-          <details className="flex ml-2" key={k}>
-            <summary>{k}</summary>
-            <Tree objectKey={k} value={v} />{" "}
-          </details>
+          <Tree key={k} objectKey={k} value={v} />
         ))}
-      </div>
+      </details>
     );
   }
   return (
-    <div className="flex ml-2">
-      {objectKey}: {value}
+    <div className="flex ml-2 items-center">
+      {objectKey}:
+      {/* {typeof value === "number" ? <Hash /> : <ALargeSmall />} */}
+      <span className="text-green-600 ml-2 ">
+        {'"'}
+        {value}
+        {'"'}
+      </span>
     </div>
   );
 }
